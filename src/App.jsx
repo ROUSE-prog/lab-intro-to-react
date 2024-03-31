db0d5431490c58440e21712ecdc48151031a406d
ROUSE-prog <stevenr2049@gmail.com>
2024-03-31 14:06:14 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./index.css";
+import NavBar from "./Components/NavBar";
+import UserProfile from "./Components/UserProfile";
+import Contacts from "./Components/Contacts";
+import Posts from "./Components/Posts";
 
 function App() {
-  return <p>Hello, world!</p>;
+  return (
+    <>
+      <NavBar />
+      <div className="w-screen h-[90vh] grid grid-cols-[4fr_2fr] grid-rows-3">
+      <UserProfile />
+      <Posts/>
+      <Contacts/>
+      </div>
+    </>
+  )
 }
 
 export default App;
